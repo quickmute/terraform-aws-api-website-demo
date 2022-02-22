f3e1b268ca757770ddcb3ea1d5dcd1af1f96ca51
Hyon <hyon24@gmail.com>
2022-02-21 18:40:39 -0600

    Create lambda.tf

---
+++ lambda.tf
@@ -0,0 +1,32 @@
+data "archive_file" "example" {
+  type        = "zip"
+  output_path = "${path.module}/lambda/example.zip"
+  source_dir  = "${path.module}/lambda/files"
+}
+
+resource "aws_lambda_function" "example" {
+  filename      = "${path.module}/lambda/example.zip"
+  function_name = var.lambda_name
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "myCalc.handler"
+
+  source_code_hash = data.archive_file.example.output_base64sha256
+
+  runtime = "nodejs12.x"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.example.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "arn:aws:execute-api:${local.regionName}:${local.accountId}:${aws_api_gateway_rest_api.example.id}/*/${aws_api_gateway_method.post.http_method}${aws_api_gateway_resource.example.path}"
+}
