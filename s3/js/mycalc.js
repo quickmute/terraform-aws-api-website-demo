bf2849ca6ed3ef5a5f4502c9c714bfe8f4faf0e3
Hyon <hyon24@gmail.com>
2022-02-21 18:45:12 -0600

    Create mycalc.js

---
+++ s3/js/mycalc.js
@@ -0,0 +1,41 @@
+(function myScopeWrapper($) {
+    $(function onDocReady() {
+  alert('Welcome');
+  $('#enterBtn').click(handleRequestClick);
+
+        if (!_config.api.invokeUrl) {
+            $('#noApiMessage').show();
+        }
+    });
+
+ function handleRequestClick(){
+  var argument = $('#argument').val();
+        calc(argument);
+ }
+ 
+ function calc(argument_var){
+   $.ajax({
+            method: 'POST',
+            url: _config.api.invokeUrl + '/mycalc',
+            headers: {
+            
+            },
+            data: JSON.stringify({
+                argument: argument_var
+            }),
+            contentType: 'application/json',
+            success: completeRequest,
+            error: completeRequest
+        });
+    }
+ 
+    function completeRequest(result) {
+        console.log('Response received from API: ', result);
+        displayUpdate(result.Response);
+    }
+
+    function displayUpdate(text) {
+        $('#updates').append($('<li>' + text + '</li>'));
+    } 
+ 
+}(jQuery));
